286b23bf9b1aca6b2b201a070c13f1cd8f8c6b19
Jakob Schlyter <jakob@kirei.se>
2018-08-26 20:38:14 +0200

    rework Scoutnet to Google mapping; now one Google group per mailing list alias

--- scoutnet2google.py
+++ scoutnet2google.py
@@ -8,6 +8,7 @@ import logging
 import re
 import sys
 import time
+from dataclasses import dataclass, field
 from distutils.util import strtobool
 import requests
 import googleapiclient.discovery
@@ -38,44 +39,28 @@ CLIENT_SECRETS_FILE = "client_secret.json"
 CLIENT_TOKEN_FILE = "client_token.json"
 MAX_RESULTS = 100
 CREATE_NAP = 10
-SCOUTNET_RE_FILTER = '^scoutnet-'
+SCOUTNET_RE_FILTER = '.*\(Scoutnet\)$'
+SCOUTNET_TAG = '(Scoutnet)'
 
 LOGGER = logging.getLogger('scoutnet2google')
 
 
-class Mailinglist:
+@dataclass(frozen=True)
+class ScoutnetMailinglist:
+    id: str
+    title: str = None
+    description: str = None
+    aliases: List[str] = field(default_factory=list)
+    members: List[str] = field(default_factory=list)
 
-    def __init__(self,
-                 unique_id: str,
-                 domain: str,
-                 members: List[str],
-                 aliases: List[str] = [],
-                 title: Optional[str] = None,
-                 description: Optional[str] = None) -> None:
-        self.unique_id = unique_id
-        self.members = set(members)
-        self.aliases = set(x.lower() for x in aliases)
-        if title is not None:
-            self.title = f"{title} (Scoutnet)"
-        else:
-            self.title = f"{unique_id} (Scoutnet)"
-        self.description = description
-        scoutnet_address = f"scoutnet-{unique_id}@{domain}"
-        self.all_addresses = list(self.aliases) + [scoutnet_address]
-        # use single alias group address, otherwise use generated address
-        self.group_address = list(self.aliases)[0] if len(self.aliases) == 1 else scoutnet_address
-        self.group_aliases = list(set(self.all_addresses) - set([self.group_address]))
 
-    def export(self) -> {}:
-        """Return mailinglist as dictionary"""
-        return {
-            'id': self.unique_id,
-            'title': self.title,
-            'description': self.description,
-            'address': self.group_address,
-            'aliases': list(self.group_aliases),
-            'members': list(self.members),
-        }
+@dataclass(frozen=True)
+class GoogleGroup:
+    address: str
+    aliases: List[str] = field(default_factory=list)
+    members: List[str] = field(default_factory=list)
+    title: str = None
+    description: str = None
 
 
 class Scoutnet(object):
@@ -90,7 +75,7 @@ class Scoutnet(object):
         response = self.session.get('{}/group/customlists'.format(self.endpoint))
         return response.json()
 
-    def get_list(self, list_data: dict) -> Mailinglist:
+    def get_list(self, list_data: dict) -> ScoutnetMailinglist:
         url = list_data.get('link')
         response = self.session.get(url).json()
         email_addresses = set()
@@ -106,15 +91,14 @@ class Scoutnet(object):
                         email_addresses.add(email.lower())
         aliases = list_data.get('aliases', {})
         if len(aliases) > 0:
-            aliases = list(aliases.values())
+            aliases = list(set(aliases.values()))
         else:
             aliases = []
-        return Mailinglist(unique_id=list_data['list_email_key'],
-                           domain=self.domain,
-                           members=list(email_addresses),
-                           aliases=aliases,
-                           title=list_data.get('title'),
-                           description=list_data.get('description'))
+        return ScoutnetMailinglist(id=list_data['list_email_key'],
+                                   members=list(email_addresses),
+                                   aliases=aliases,
+                                   title=list_data.get('title'),
+                                   description=list_data.get('description'))
 
 
 class GoogleDirectory(object):
@@ -123,33 +107,36 @@ class GoogleDirectory(object):
         self.service = service
         self.domain = domain
         self.logger = LOGGER.getChild('GoogleDirectory')
+        self.dry_run = True
 
-    def sync_mlists(self, lists: List[Mailinglist]) -> None:
+    def sync_groups(self, groups: List[GoogleGroup]) -> None:
         """Syncronize mailing lists with Google"""
-        self.delete_removed_groups(lists)
-        for mlist in lists:
-            self.logger.info("Synchronizing group %s aka %s", mlist.group_address, mlist.group_aliases)
-            group_key = mlist.group_address
-            self.sync_group_mlist(group_key, mlist)
-            self.sync_group_members(group_key, mlist.members)
+        self.delete_removed_groups(groups)
+        for group in groups:
+            self.logger.info("Synchronizing group %s", group.address)
+            self.sync_group_info(group)
+            self.sync_group_aliases(group)
+            self.sync_group_members(group)
 
-    def delete_removed_groups(self, lists: List[Mailinglist]) -> None:
+    def delete_removed_groups(self, groups: List[GoogleGroup]) -> None:
         current_groups = set(self.get_all_groups(SCOUTNET_RE_FILTER))
-        old_groups = current_groups - set([list.group_address for list in lists])
+        old_groups = current_groups - set([group.address for group in groups])
         for group_key in old_groups:
             self.logger.info("Deleting group %s", group_key)
-            self.service.groups().delete(groupKey=group_key).execute()
+            if not self.dry_run:
+                self.service.groups().delete(groupKey=group_key).execute()
 
-    def sync_group_mlist(self, group_key: str, mlist: Mailinglist) -> None:
+    def sync_group_info(self, group: GoogleGroup) -> None:
         """Update/create group information"""
+        group_key = group.address
         group_body = {
-            'email': mlist.group_address,
-            'name': mlist.title,
-            'description': re.sub("\n|\r", "", mlist.description.strip())
+            'email': group.address,
+            'name': group.title,
+            'description': group.description
         }
         try:
             result = self.service.groups().get(groupKey=group_key).execute()
-            if result.get('name') == mlist.title and result.get('description') == mlist.description:
+            if result.get('name') == group.title and result.get('description') == group.description:
                 self.logger.info("Group %s up to date", group_key)
             else:
                 result = self.service.groups().update(groupKey=group_key, body=group_body).execute()
@@ -169,25 +156,30 @@ class GoogleDirectory(object):
                 group = self.service.groups().get(groupKey=group_key).execute()
             self.logger.info("Group %s created", group_key)
 
+    def sync_group_aliases(self, group: GoogleGroup) -> None:
+        """Update/create group information"""
+        group_key = group.address
         result = self.service.groups().aliases().list(groupKey=group_key).execute()
         if result is not None:
             current_group_aliases = set(entry['alias'] for entry in result.get('aliases', []))
         else:
             current_group_aliases = set()
-        for alias in set(mlist.group_aliases) - current_group_aliases:
+        for alias in set(group.aliases) - current_group_aliases:
             self.logger.info("Adding alias: %s", alias)
             alias_body = {'alias': alias}
             result = self.service.groups().aliases().insert(groupKey=group_key, body=alias_body).execute()
             print(result)
-        for alias in current_group_aliases - set(mlist.group_aliases):
+        for alias in current_group_aliases - set(group.aliases):
             self.logger.info("Removing alias: %s", alias)
             result = self.service.groups().aliases().delete(groupKey=group_key, alias=alias).execute()
             print(result)
 
-    def sync_group_members(self, group_key: str, list_members: Set[str]) -> None:
+    def sync_group_members(self, group: GoogleGroup) -> None:
+        group_key = group.address
+        members = set(group.members)
         current_members = set(self.get_all_members(group_key))
-        new_members = list_members - current_members
-        old_members = current_members - list_members
+        new_members = members - current_members
+        old_members = current_members - members
         self.logger.debug(f"Current group members: {current_members}")
         self.logger.debug(f"New group members: {new_members}")
         self.logger.debug(f"Old group members: {old_members}")
@@ -216,16 +208,12 @@ class GoogleDirectory(object):
             result = self.service.groups().list(domain=self.domain, pageToken=token, maxResults=max_results).execute()
             for group in result.get('groups', []):
                 group_address = group['email']
-                ignore_group = True
-                if re.match(re_filter, group_address):
-                    ignore_group = False
-                for alias in group.get('aliases', []):
-                    if re.match(re_filter, alias):
-                        ignore_group = False
-                if not ignore_group:
+                group_name = group['name']
+                if re.match(re_filter, group_name):
+                    self.logger.debug("Including group %s", group_address)
                     all_groups.append(group_address)
                 else:
-                    self.logger.info("Group %s ignored", group_address)
+                    self.logger.info("Excluding group %s", group_address)
             token = result.get('nextPageToken')
             if token is None:
                 break
@@ -272,6 +260,25 @@ def google_auth_installed(secret_file: str, token_file: str, scopes: List[str]) 
             json.dump(token_data, token_file)
         logger.info("Credentials saved to %s", token_file)
     return credentials
+
+
+def mailinglist2groups(mlist: ScoutnetMailinglist) -> List[GoogleGroup]:
+    """Convert Scoutnet mailinglist to Google groups"""
+    groups = []
+    for address in mlist.aliases:
+        if mlist.title is not None:
+            title = f"{mlist.title} {SCOUTNET_TAG}"
+        else:
+            title = f"{mlist.id} {SCOUTNET_TAG}"
+        if mlist.description is not None:
+            description = re.sub("\n|\r|=", "", mlist.description.strip())
+        else:
+            description = None
+        groups.append(GoogleGroup(address=address,
+                                  members=mlist.members,
+                                  title=title,
+                                  description=description))
+    return groups
 
 
 def main() -> None:
@@ -337,24 +344,28 @@ def main() -> None:
     for (clist, cdata) in scoutnet.customlists().items():
         count += 1
         mlist = scoutnet.get_list(cdata)
-        LOGGER.info("Fetched %s: %s", mlist.unique_id, mlist.title)
-        if include_generic or len(mlist.aliases) > 0:
-            LOGGER.debug("Including %s: %s", mlist.unique_id, mlist.title)
+        LOGGER.info("Fetched %s: %s", mlist.id, mlist.title)
+        if len(mlist.aliases) > 0:
+            LOGGER.debug("Including %s: %s", mlist.id, mlist.title)
             all_lists.append(mlist)
         else:
-            LOGGER.debug("Excluding %s: %s", mlist.unique_id, mlist.title)
+            LOGGER.debug("Excluding %s: %s", mlist.id, mlist.title)
         if limit is not None and count >= limit:
             break
 
     # Optionally output all groups to file
     if args.output:
-        groups = [mlist.export() for mlist in all_lists]
         with open(args.output, 'wt') as file:
-            file.write(json.dumps(groups, sort_keys=True, indent=4))
+            file.write(json.dumps([x.__dict__ for x in all_lists], sort_keys=True, indent=4))
+
+    # Convert Scoutnet mailinglists to Google groups
+    all_groups = []
+    for mlist in all_lists:
+        all_groups.extend(mailinglist2groups(mlist))
 
     # Syncronize with Google Directory
     if not args.skip_google:
-        directory.sync_mlists(all_lists)
+        directory.sync_groups(all_groups)
 
 
 if __name__ == "__main__":
